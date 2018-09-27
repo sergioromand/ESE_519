f269eee9c550b9056809d1d962f4ac7af0fa2414
sergioromand <sergioromandurini@gmail.com>
2018-09-27 17:12:23 -0400

    Lab 3 code complete

---
+++ lab_03/main.c
@@ -0,0 +1,45 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <util/delay.h>
+#include "lcd.h"
+
+#define FREQ 16000000
+#define BAUD 9600
+#define HIGH 1
+#define LOW 0
+#define BUFFER 1024
+#define BLACK 0x000001
+
+char displayChar = 0;
+
+int main(void)
+{
+	//setting up the gpio for backlight
+	DDRD |= 0x80;
+	PORTD &= ~0x80;
+	PORTD |= 0x00;
+	
+	DDRB |= 0x05;
+	PORTB &= ~0x05;
+	PORTB |= 0x00;
+	
+	//lcd initialisation
+	lcd_init();
+	lcd_command(CMD_DISPLAY_ON);
+	lcd_set_brightness(0x18);
+	write_buffer(buff);
+	_delay_ms(10000);
+	clear_buffer(buff);
+	
+	while (1)
+	{
+		drawchar(buff,0,0,displayChar);
+		write_buffer(buff);
+		_delay_ms(5000);
+		displayChar++;
+	}
+}
