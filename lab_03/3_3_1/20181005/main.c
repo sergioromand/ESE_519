817fce5d642c627f8141d655f7cccc62d80edc87
yatesyatesyates <42921249+yatesyatesyates@users.noreply.github.com>
2018-10-05 00:55:48 -0400

    Add files via upload

---
+++ lab_03/3_3_1/20181005/main.c
@@ -0,0 +1,88 @@
+/*
+ * 3_3_1.c
+ *
+ * Created: 10/3/2018 4:36:25 AM
+ * Author : yatesyatesyates
+ */ 
+
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <util/delay.h>
+#include "lcd.h"
+#include "uart.h"
+
+#define FREQ 16000000
+#define BAUD 9600
+#define HIGH 1
+#define LOW 0
+#define BUFFER 1024
+#define BLACK 0x000001
+
+char displayChar = 1;
+int touch;
+
+
+
+int main(void)
+{
+	uart_init();
+	 
+	//setting up the gpio for backlight
+	DDRD |= 0x80;  // define PD7 as output
+	DDRB |= 0x05;  // define PB0,PB2 as output
+
+
+	lcd_kellygreen();
+
+	
+	//lcd initialization
+	lcd_init();
+	lcd_command(CMD_DISPLAY_ON);
+	lcd_set_brightness(0x18);
+	clear_buffer(buff);
+	
+	read_screen_init();
+	
+	init_play();
+	
+	while (1)
+	{	
+		
+		/*  working prototype 10.04.18	
+		touch = standby_mode();
+		if (touch){
+			move_paddle();	
+			write_buffer(buff);
+			}	
+	*/
+		
+		draw_game_borders();
+		write_buffer(buff);
+		
+		/*touch = standby_mode();
+		if (touch){
+			//	determine "A side touch" or "B side touch" 
+			//		update next "y" for draw_paddleA or next "y" for draw_paddleB
+		}
+		if (ball contact){
+			//	check for ball contact with wall or paddle
+			//		update next "x" and "y" for ball 
+		}
+		if (goal){
+			//	check for goal
+			//		if goal then increment appropriate score and init play
+			//		if someone's score is 10 then celebrate winner and reset score
+		}
+			//			move_paddle();
+			//			move_ball();
+			//			write_buffer(buff);
+		}*/	
+		
+	}
+}
+
